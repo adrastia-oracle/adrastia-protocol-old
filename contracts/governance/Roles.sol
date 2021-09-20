a10dcc020bc1d0979aa12778b10696ef95b04494
Tyler Loewen <tyler@trilez.com>
2021-09-19 18:23:23 -0700

    Update governance roles

--- contracts/governance/Roles.sol
+++ contracts/governance/Roles.sol
@@ -6,7 +6,16 @@ library Roles {
 
     bytes32 public constant ADMIN = keccak256("ADMIN_ROLE");
 
-    bytes32 public constant INCENTIVE_MAINTAINER = keccak256("INCENTIVE_MAINTAINER");
+    bytes32 public constant INCENTIVE_MAINTAINER = keccak256("INCENTIVE_MAINTAINER_ROLE");
 
-    bytes32 public constant WHITELIST_MAINTAINER = keccak256("WHITELIST_MAINTAINER");
+    bytes32 public constant WHITELIST_MAINTAINER = keccak256("WHITELIST_MAINTAINER_ROLE");
+
+    /*
+     * Timelock roles
+     */
+    bytes32 public constant TIMELOCK_ADMIN = keccak256("TIMELOCK_ADMIN_ROLE");
+
+    bytes32 public constant TIMELOCK_PROPOSER = keccak256("TIMELOCK_PROPOSER_ROLE");
+
+    bytes32 public constant TIMELOCK_EXECUTOR = keccak256("TIMELOCK_EXECUTOR_ROLE");
 }
