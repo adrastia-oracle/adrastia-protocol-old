1c8891b2007c25a376e2cd3c28e3cef9cf0edbe5
Tyler Loewen <tyler@trilez.com>
2021-09-20 18:47:26 -0700

    Inline governance extensions - The inheritance graph was so hard to follow. Too many different files. Hard to read. - This greatly improves the readability of the governance code.

--- contracts/governance/Board.sol
+++ contracts/governance/Board.sol
@@ -1,29 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
 import "@openzeppelin/contracts-upgradeable/governance/GovernorUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorCountingSimpleUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorVotesUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorVotesQuorumFractionUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorTimelockControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/governance/TimelockControllerUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/governance/extensions/IGovernorTimelockUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 import "./Roles.sol";
 
 contract BoardStorageV1 {
+    /// @dev Supported vote types. Matches Governor Bravo ordering.
+    enum VoteType {
+        Against,
+        For,
+        Abstain
+    }
+
+    struct ProposalVote {
+        uint256 againstVotes;
+        uint256 forVotes;
+        uint256 abstainVotes;
+        mapping(address => bool) hasVoted;
+    }
+
+    ERC20Votes public voteToken;
+
     uint256 internal _votingDelay;
     uint256 internal _votingPeriod;
+
+    uint256 internal _quorumNumerator;
+    uint256 internal _quorumDenominator;
+
+    mapping(uint256 => ProposalVote) internal _proposalVotes;
+
+    TimelockControllerUpgradeable internal _timelock;
+    mapping(uint256 => bytes32) internal _timelockIds;
+
+    event TimelockChange(address oldTimelock, address newTimelock);
 }
 
 contract Board is
     Initializable,
     GovernorUpgradeable,
-    GovernorCountingSimpleUpgradeable,
-    GovernorVotesUpgradeable,
-    GovernorVotesQuorumFractionUpgradeable,
-    GovernorTimelockControlUpgradeable,
+    IGovernorTimelockUpgradeable,
     AccessControlEnumerableUpgradeable,
     UUPSUpgradeable,
     BoardStorageV1
@@ -34,27 +57,54 @@ contract Board is
 
     function initialize(
         string memory name_,
-        ERC20VotesUpgradeable token_,
+        ERC20Votes token_,
         TimelockControllerUpgradeable timelock_,
         uint256 quorumFraction_,
         uint256 votingDelay_,
         uint256 votingPeriod_
     ) public initializer {
         __Governor_init(name_);
-        __GovernorCountingSimple_init();
-        __GovernorVotes_init(token_);
-        __GovernorVotesQuorumFraction_init(quorumFraction_);
-        __GovernorTimelockControl_init(timelock_);
         __AccessControlEnumerable_init();
         __UUPSUpgradeable_init();
 
+        voteToken = token_;
+
         _votingPeriod = votingPeriod_;
         _votingDelay = votingDelay_;
+
+        _quorumNumerator = quorumFraction_;
+        _quorumNumerator = 100;
+
+        _timelock = timelock_;
+    }
+
+    /*
+     * External functions
+     */
+    function updateTimelock(TimelockControllerUpgradeable newTimelock) external virtual onlyRole(Roles.SUPER) {
+        emit TimelockChange(address(_timelock), address(newTimelock));
+        _timelock = newTimelock;
+    }
+
+    /*
+     * Public functions - pure
+     */
+    function COUNTING_MODE() public pure virtual override returns (string memory) {
+        return "support=bravo&quorum=for,abstain";
     }
 
     /*
      * Public functions - view
      */
+
+    function timelock() public view virtual override returns (address) {
+        return address(_timelock);
+    }
+
+    function proposalEta(uint256 proposalId) public view virtual override returns (uint256) {
+        uint256 eta = _timelock.getTimestamp(_timelockIds[proposalId]);
+        return eta == 1 ? 0 : eta; // _DONE_TIMESTAMP (1) should be replaced with a 0 value
+    }
 
     function votingDelay() public view override returns (uint256) {
         return _votingDelay;
@@ -64,31 +114,54 @@ contract Board is
         return _votingPeriod;
     }
 
-    function quorum(uint256 blockNumber)
-        public
-        view
-        override(IGovernorUpgradeable, GovernorVotesQuorumFractionUpgradeable)
-        returns (uint256)
-    {
-        return super.quorum(blockNumber);
+    function hasVoted(uint256 proposalId, address account) public view virtual override returns (bool) {
+        return _proposalVotes[proposalId].hasVoted[account];
     }
 
-    function getVotes(address account, uint256 blockNumber)
+    function proposalVotes(uint256 proposalId)
         public
         view
-        override(IGovernorUpgradeable, GovernorVotesUpgradeable)
-        returns (uint256)
+        virtual
+        returns (
+            uint256 againstVotes,
+            uint256 forVotes,
+            uint256 abstainVotes
+        )
     {
-        return super.getVotes(account, blockNumber);
+        ProposalVote storage proposalvote = _proposalVotes[proposalId];
+        return (proposalvote.againstVotes, proposalvote.forVotes, proposalvote.abstainVotes);
+    }
+
+    function quorum(uint256 blockNumber) public view override returns (uint256) {
+        return (voteToken.getPastTotalSupply(blockNumber) * _quorumNumerator) / _quorumDenominator;
+    }
+
+    function getVotes(address account, uint256 blockNumber) public view override returns (uint256) {
+        return voteToken.getPastVotes(account, blockNumber);
     }
 
     function state(uint256 proposalId)
         public
         view
-        override(GovernorUpgradeable, GovernorTimelockControlUpgradeable)
+        virtual
+        override(IGovernorUpgradeable, GovernorUpgradeable)
         returns (ProposalState)
     {
-        return super.state(proposalId);
+        ProposalState status = super.state(proposalId);
+
+        if (status != ProposalState.Succeeded) {
+            return status;
+        }
+
+        // core tracks execution, so we just have to check if successful proposal have been queued.
+        bytes32 queueid = _timelockIds[proposalId];
+        if (queueid == bytes32(0)) {
+            return status;
+        } else if (_timelock.isOperationDone(queueid)) {
+            return ProposalState.Executed;
+        } else {
+            return ProposalState.Queued;
+        }
     }
 
     /*
@@ -98,10 +171,10 @@ contract Board is
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(GovernorUpgradeable, GovernorTimelockControlUpgradeable, AccessControlEnumerableUpgradeable)
+        override(GovernorUpgradeable, AccessControlEnumerableUpgradeable, IERC165Upgradeable)
         returns (bool)
     {
-        return super.supportsInterface(interfaceId);
+        return interfaceId == type(IGovernorTimelockUpgradeable).interfaceId || super.supportsInterface(interfaceId);
     }
 
     /*
@@ -113,21 +186,35 @@ contract Board is
         uint256[] memory values,
         bytes[] memory calldatas,
         string memory description
-    ) public override(GovernorUpgradeable, IGovernorUpgradeable) returns (uint256) {
+    ) public override(IGovernorUpgradeable, GovernorUpgradeable) returns (uint256) {
         return super.propose(targets, values, calldatas, description);
+    }
+
+    function queue(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    ) public virtual override returns (uint256) {
+        uint256 proposalId = hashProposal(targets, values, calldatas, descriptionHash);
+
+        require(state(proposalId) == ProposalState.Succeeded, "Governor: proposal not successful");
+
+        uint256 delay = _timelock.getMinDelay();
+        _timelockIds[proposalId] = _timelock.hashOperationBatch(targets, values, calldatas, 0, descriptionHash);
+        _timelock.scheduleBatch(targets, values, calldatas, 0, descriptionHash, delay);
+
+        emit ProposalQueued(proposalId, block.timestamp + delay);
+
+        return proposalId;
     }
 
     /*
      * Internal functions - view
      */
 
-    function _executor()
-        internal
-        view
-        override(GovernorUpgradeable, GovernorTimelockControlUpgradeable)
-        returns (address)
-    {
-        return super._executor();
+    function _executor() internal view override(GovernorUpgradeable) returns (address) {
+        return address(_timelock);
     }
 
     /*
@@ -135,13 +222,13 @@ contract Board is
      */
 
     function _execute(
-        uint256 proposalId,
+        uint256, /* proposalId */
         address[] memory targets,
         uint256[] memory values,
         bytes[] memory calldatas,
         bytes32 descriptionHash
-    ) internal override(GovernorUpgradeable, GovernorTimelockControlUpgradeable) {
-        super._execute(proposalId, targets, values, calldatas, descriptionHash);
+    ) internal virtual override {
+        _timelock.executeBatch{value: msg.value}(targets, values, calldatas, 0, descriptionHash);
     }
 
     function _cancel(
@@ -149,8 +236,55 @@ contract Board is
         uint256[] memory values,
         bytes[] memory calldatas,
         bytes32 descriptionHash
-    ) internal override(GovernorUpgradeable, GovernorTimelockControlUpgradeable) returns (uint256) {
-        return super._cancel(targets, values, calldatas, descriptionHash);
+    ) internal virtual override returns (uint256) {
+        uint256 proposalId = super._cancel(targets, values, calldatas, descriptionHash);
+
+        if (_timelockIds[proposalId] != 0) {
+            _timelock.cancel(_timelockIds[proposalId]);
+            delete _timelockIds[proposalId];
+        }
+
+        return proposalId;
+    }
+
+    function _quorumReached(uint256 proposalId) internal view virtual override returns (bool) {
+        ProposalVote storage proposalvote = _proposalVotes[proposalId];
+
+        return quorum(proposalSnapshot(proposalId)) <= proposalvote.forVotes + proposalvote.abstainVotes;
+    }
+
+    /**
+     * @dev See {Governor-_voteSucceeded}. In this module, the forVotes must be scritly over the againstVotes.
+     */
+    function _voteSucceeded(uint256 proposalId) internal view virtual override returns (bool) {
+        ProposalVote storage proposalvote = _proposalVotes[proposalId];
+
+        return proposalvote.forVotes > proposalvote.againstVotes;
+    }
+
+    /**
+     * @dev See {Governor-_countVote}. In this module, the support follows the `VoteType` enum (from Governor Bravo).
+     */
+    function _countVote(
+        uint256 proposalId,
+        address account,
+        uint8 support,
+        uint256 weight
+    ) internal virtual override {
+        ProposalVote storage proposalvote = _proposalVotes[proposalId];
+
+        require(!proposalvote.hasVoted[account], "GovernorVotingSimple: vote already cast");
+        proposalvote.hasVoted[account] = true;
+
+        if (support == uint8(VoteType.Against)) {
+            proposalvote.againstVotes += weight;
+        } else if (support == uint8(VoteType.For)) {
+            proposalvote.forVotes += weight;
+        } else if (support == uint8(VoteType.Abstain)) {
+            proposalvote.abstainVotes += weight;
+        } else {
+            revert("GovernorVotingSimple: invalid value for enum VoteType");
+        }
     }
 
     /*
