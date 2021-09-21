35c69370f79c5050701b1bd12543f436164c3fc7
Tyler Loewen <tyler@trilez.com>
2021-09-21 14:56:48 -0700

    Rename permissionManager to roleManager in RoleConsumer

--- contracts/access/RoleConsumer.sol
+++ contracts/access/RoleConsumer.sol
@@ -11,22 +11,22 @@ abstract contract RoleConsumer is IAccessControlEnumerable {
         _;
     }
 
-    function permissionManager() public view virtual returns (RoleManager);
+    function roleManager() public view virtual returns (RoleManager);
 
     function getRoleMember(bytes32 role, uint256 index) public view override returns (address) {
-        return permissionManager().getRoleMember(role, index);
+        return roleManager().getRoleMember(role, index);
     }
 
     function getRoleMemberCount(bytes32 role) public view override returns (uint256) {
-        return permissionManager().getRoleMemberCount(role);
+        return roleManager().getRoleMemberCount(role);
     }
 
     function hasRole(bytes32 role, address account) public view override returns (bool) {
-        return permissionManager().hasRole(role, account);
+        return roleManager().hasRole(role, account);
     }
 
     function getRoleAdmin(bytes32 role) public view override returns (bytes32) {
-        return permissionManager().getRoleAdmin(role);
+        return roleManager().getRoleAdmin(role);
     }
 
     function grantRole(
