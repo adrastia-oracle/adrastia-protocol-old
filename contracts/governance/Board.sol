877cff2cc6c7daae0dd3f99855e63f86082a207f
Tyler Loewen <tyler@trilez.com>
2021-09-20 21:50:53 -0700

    Inline GovernorUpgradeable - Readability greatly improved

--- contracts/governance/Board.sol
+++ contracts/governance/Board.sol
@@ -3,7 +3,14 @@ pragma solidity ^0.8;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-import "@openzeppelin/contracts-upgradeable/governance/GovernorUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/introspection/ERC165Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeCastUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/TimersUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/governance/IGovernorUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/governance/TimelockControllerUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/governance/extensions/IGovernorTimelockUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
@@ -27,6 +34,17 @@ contract BoardStorageV1 {
         mapping(address => bool) hasVoted;
     }
 
+    struct ProposalCore {
+        TimersUpgradeable.BlockNumber voteStart;
+        TimersUpgradeable.BlockNumber voteEnd;
+        bool executed;
+        bool canceled;
+    }
+
+    string internal _name;
+
+    uint256 internal _version;
+
     ERC20Votes public voteToken;
 
     uint256 internal _votingDelay;
@@ -35,6 +53,7 @@ contract BoardStorageV1 {
     uint256 internal _quorumNumerator;
     uint256 internal _quorumDenominator;
 
+    mapping(uint256 => ProposalCore) internal _proposals;
     mapping(uint256 => ProposalVote) internal _proposalVotes;
 
     TimelockControllerUpgradeable internal _timelock;
@@ -45,12 +64,20 @@ contract BoardStorageV1 {
 
 contract Board is
     Initializable,
-    GovernorUpgradeable,
+    ContextUpgradeable,
+    ERC165Upgradeable,
+    EIP712Upgradeable,
+    IGovernorUpgradeable,
     IGovernorTimelockUpgradeable,
     AccessControlEnumerableUpgradeable,
     UUPSUpgradeable,
     BoardStorageV1
 {
+    using TimersUpgradeable for TimersUpgradeable.BlockNumber;
+    using SafeCastUpgradeable for uint256;
+
+    bytes32 public constant BALLOT_TYPEHASH = keccak256("Ballot(uint256 proposalId,uint8 support)");
+
     /*
      * Constructors and initializers
      */
@@ -63,9 +90,11 @@ contract Board is
         uint256 votingDelay_,
         uint256 votingPeriod_
     ) public initializer {
-        __Governor_init(name_);
         __AccessControlEnumerable_init();
         __UUPSUpgradeable_init();
+
+        _name = name_;
+        _version = 1;
 
         voteToken = token_;
 
@@ -96,6 +125,36 @@ contract Board is
     /*
      * Public functions - view
      */
+
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    function version() public view virtual override returns (string memory) {
+        return "1"; // TODO: int to string -> return
+    }
+
+    /**
+     * @dev See {IGovernor-hashProposal}.
+     *
+     * The proposal id is produced by hashing the RLC encoded `targets` array, the `values` array, the `calldatas` array
+     * and the descriptionHash (bytes32 which itself is the keccak256 hash of the description string). This proposal id
+     * can be produced from the proposal data which is part of the {ProposalCreated} event. It can even be computed in
+     * advance, before the proposal is submitted.
+     *
+     * Note that the chainId and the governor address are not part of the proposal id computation. Consequently, the
+     * same proposal (with same operation and same description) will have the same id if submitted on multiple governors
+     * accross multiple networks. This also means that in order to execute the same operation twice (on the same
+     * governor) the proposer will have to change the description in order to avoid proposal id conflicts.
+     */
+    function hashProposal(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    ) public pure virtual override returns (uint256) {
+        return uint256(keccak256(abi.encode(targets, values, calldatas, descriptionHash)));
+    }
 
     function timelock() public view virtual override returns (address) {
         return address(_timelock);
@@ -140,14 +199,8 @@ contract Board is
         return voteToken.getPastVotes(account, blockNumber);
     }
 
-    function state(uint256 proposalId)
-        public
-        view
-        virtual
-        override(IGovernorUpgradeable, GovernorUpgradeable)
-        returns (ProposalState)
-    {
-        ProposalState status = super.state(proposalId);
+    function state(uint256 proposalId) public view virtual override returns (ProposalState) {
+        ProposalState status = _state(proposalId);
 
         if (status != ProposalState.Succeeded) {
             return status;
@@ -164,6 +217,14 @@ contract Board is
         }
     }
 
+    function proposalSnapshot(uint256 proposalId) public view virtual override returns (uint256) {
+        return _proposals[proposalId].voteStart.getDeadline();
+    }
+
+    function proposalDeadline(uint256 proposalId) public view virtual override returns (uint256) {
+        return _proposals[proposalId].voteEnd.getDeadline();
+    }
+
     /*
      * Public functions - view - ERC165
      */
@@ -171,23 +232,82 @@ contract Board is
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(GovernorUpgradeable, AccessControlEnumerableUpgradeable, IERC165Upgradeable)
+        override(AccessControlEnumerableUpgradeable, ERC165Upgradeable, IERC165Upgradeable)
         returns (bool)
     {
-        return interfaceId == type(IGovernorTimelockUpgradeable).interfaceId || super.supportsInterface(interfaceId);
+        return
+            interfaceId == type(IGovernorUpgradeable).interfaceId ||
+            interfaceId == type(IGovernorTimelockUpgradeable).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 
     /*
      * Public functions
      */
+    function castVote(uint256 proposalId, uint8 support) public virtual override returns (uint256) {
+        address voter = _msgSender();
+        return _castVote(proposalId, voter, support, "");
+    }
+
+    function castVoteWithReason(
+        uint256 proposalId,
+        uint8 support,
+        string calldata reason
+    ) public virtual override returns (uint256) {
+        address voter = _msgSender();
+        return _castVote(proposalId, voter, support, reason);
+    }
+
+    function castVoteBySig(
+        uint256 proposalId,
+        uint8 support,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public virtual override returns (uint256) {
+        address voter = ECDSAUpgradeable.recover(
+            _hashTypedDataV4(keccak256(abi.encode(BALLOT_TYPEHASH, proposalId, support))),
+            v,
+            r,
+            s
+        );
+        return _castVote(proposalId, voter, support, "");
+    }
 
     function propose(
         address[] memory targets,
         uint256[] memory values,
         bytes[] memory calldatas,
         string memory description
-    ) public override(IGovernorUpgradeable, GovernorUpgradeable) returns (uint256) {
-        return super.propose(targets, values, calldatas, description);
+    ) public override returns (uint256) {
+        uint256 proposalId = hashProposal(targets, values, calldatas, keccak256(bytes(description)));
+
+        require(targets.length == values.length, "Governor: invalid proposal length");
+        require(targets.length == calldatas.length, "Governor: invalid proposal length");
+        require(targets.length > 0, "Governor: empty proposal");
+
+        ProposalCore storage proposal = _proposals[proposalId];
+        require(proposal.voteStart.isUnset(), "Governor: proposal already exists");
+
+        uint64 snapshot = block.number.toUint64() + votingDelay().toUint64();
+        uint64 deadline = snapshot + votingPeriod().toUint64();
+
+        proposal.voteStart.setDeadline(snapshot);
+        proposal.voteEnd.setDeadline(deadline);
+
+        emit ProposalCreated(
+            proposalId,
+            _msgSender(),
+            targets,
+            values,
+            new string[](targets.length),
+            calldatas,
+            snapshot,
+            deadline,
+            description
+        );
+
+        return proposalId;
     }
 
     function queue(
@@ -209,45 +329,58 @@ contract Board is
         return proposalId;
     }
 
-    /*
-     * Internal functions - view
-     */
-
-    function _executor() internal view override(GovernorUpgradeable) returns (address) {
-        return address(_timelock);
-    }
-
-    /*
-     * Internal functions
-     */
-
-    function _execute(
-        uint256, /* proposalId */
+    function execute(
         address[] memory targets,
         uint256[] memory values,
         bytes[] memory calldatas,
         bytes32 descriptionHash
-    ) internal virtual override {
-        _timelock.executeBatch{value: msg.value}(targets, values, calldatas, 0, descriptionHash);
-    }
+    ) public payable virtual override returns (uint256) {
+        uint256 proposalId = hashProposal(targets, values, calldatas, descriptionHash);
 
-    function _cancel(
-        address[] memory targets,
-        uint256[] memory values,
-        bytes[] memory calldatas,
-        bytes32 descriptionHash
-    ) internal virtual override returns (uint256) {
-        uint256 proposalId = super._cancel(targets, values, calldatas, descriptionHash);
+        ProposalState status = state(proposalId);
+        require(
+            status == ProposalState.Succeeded || status == ProposalState.Queued,
+            "Governor: proposal not successful"
+        );
+        _proposals[proposalId].executed = true;
 
-        if (_timelockIds[proposalId] != 0) {
-            _timelock.cancel(_timelockIds[proposalId]);
-            delete _timelockIds[proposalId];
-        }
+        emit ProposalExecuted(proposalId);
+
+        _execute(proposalId, targets, values, calldatas, descriptionHash);
 
         return proposalId;
     }
 
-    function _quorumReached(uint256 proposalId) internal view virtual override returns (bool) {
+    /*
+     * Internal functions - view
+     */
+
+    function _executor() internal view returns (address) {
+        return address(_timelock);
+    }
+
+    function _state(uint256 proposalId) internal view virtual returns (ProposalState) {
+        ProposalCore memory proposal = _proposals[proposalId];
+
+        if (proposal.executed) {
+            return ProposalState.Executed;
+        } else if (proposal.canceled) {
+            return ProposalState.Canceled;
+        } else if (proposal.voteStart.isPending()) {
+            return ProposalState.Pending;
+        } else if (proposal.voteEnd.isPending()) {
+            return ProposalState.Active;
+        } else if (proposal.voteEnd.isExpired()) {
+            return
+                _quorumReached(proposalId) && _voteSucceeded(proposalId)
+                    ? ProposalState.Succeeded
+                    : ProposalState.Defeated;
+        } else {
+            revert("Governor: unknown proposal id");
+        }
+    }
+
+    function _quorumReached(uint256 proposalId) internal view virtual returns (bool) {
         ProposalVote storage proposalvote = _proposalVotes[proposalId];
 
         return quorum(proposalSnapshot(proposalId)) <= proposalvote.forVotes + proposalvote.abstainVotes;
@@ -256,10 +389,72 @@ contract Board is
     /**
      * @dev See {Governor-_voteSucceeded}. In this module, the forVotes must be scritly over the againstVotes.
      */
-    function _voteSucceeded(uint256 proposalId) internal view virtual override returns (bool) {
+    function _voteSucceeded(uint256 proposalId) internal view virtual returns (bool) {
         ProposalVote storage proposalvote = _proposalVotes[proposalId];
 
         return proposalvote.forVotes > proposalvote.againstVotes;
+    }
+
+    /*
+     * Internal functions
+     */
+
+    /**
+     * @dev Internal vote casting mechanism: Check that the vote is pending, that it has not been cast yet, retrieve
+     * voting weight using {IGovernor-getVotes} and call the {_countVote} internal function.
+     *
+     * Emits a {IGovernor-VoteCast} event.
+     */
+    function _castVote(
+        uint256 proposalId,
+        address account,
+        uint8 support,
+        string memory reason
+    ) internal virtual returns (uint256) {
+        ProposalCore storage proposal = _proposals[proposalId];
+        require(state(proposalId) == ProposalState.Active, "Governor: vote not currently active");
+
+        uint256 weight = getVotes(account, proposal.voteStart.getDeadline());
+        _countVote(proposalId, account, support, weight);
+
+        emit VoteCast(account, proposalId, support, weight, reason);
+
+        return weight;
+    }
+
+    function _execute(
+        uint256, /* proposalId */
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    ) internal virtual {
+        _timelock.executeBatch{value: msg.value}(targets, values, calldatas, 0, descriptionHash);
+    }
+
+    function _cancel(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    ) internal virtual returns (uint256) {
+        uint256 proposalId = hashProposal(targets, values, calldatas, descriptionHash);
+        ProposalState status = state(proposalId);
+
+        require(
+            status != ProposalState.Canceled && status != ProposalState.Expired && status != ProposalState.Executed,
+            "Governor: proposal not active"
+        );
+        _proposals[proposalId].canceled = true;
+
+        emit ProposalCanceled(proposalId);
+
+        if (_timelockIds[proposalId] != 0) {
+            _timelock.cancel(_timelockIds[proposalId]);
+            delete _timelockIds[proposalId];
+        }
+
+        return proposalId;
     }
 
     /**
@@ -270,7 +465,7 @@ contract Board is
         address account,
         uint8 support,
         uint256 weight
-    ) internal virtual override {
+    ) internal virtual {
         ProposalVote storage proposalvote = _proposalVotes[proposalId];
 
         require(!proposalvote.hasVoted[account], "GovernorVotingSimple: vote already cast");
